ffd39f7f4a564d1c60e63d63692454cdeec9fd50
Ameli4ckin <iameli4ckin@mail.ru>
2022-09-16 19:45:08 +0300

    add 5 dz

--- src/MessageList/MessageList.tsx
+++ src/MessageList/MessageList.tsx
@@ -1,11 +1,11 @@
 import { List, ListItem } from '@mui/material';
 import React from 'react';
 import { FC } from 'react';
-import { Messages } from '../types';
+import { Message } from '../types';
 
 
 interface MessageListProps {
-    messages: Messages;
+    messages: Message[];
 }
 
 export const MessageList: FC<MessageListProps> = ({ messages }) => {
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,3 @@
+import React, { FC } from 'react';
+
+export const Profile: FC = () => <h2>profile page</h2>
